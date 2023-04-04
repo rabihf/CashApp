d98b9c342c55a988b72e37040843da6f5f8c085e
RabihAUB <rf28@aub.edu.lb>
2023-04-04 15:45:40 +0300

    Updated Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -24,13 +24,65 @@ namespace ConsoleApp
             Console.WriteLine($"payUSD:{payUSD}");
             Console.WriteLine($"cashUSD + payUSD: {cashUSD + payUSD}");
             Console.WriteLine($"cashUSD - payUSD: {cashUSD - payUSD}");
+            //
+            // var l = typeof(CurrencyEnum).GetEnumValues();
+            // foreach (var obj in l)
+            // {
+            //     Console.WriteLine(obj);
+            // }
+            //
+            // CashEnum test = CashEnum.HundredThousand;
+            // Console.WriteLine($"CheckEnum(test): {CheckEnum(test)}");
+            // Console.WriteLine($"test: {test}");
+            // Console.WriteLine($"(int)test: {(int)test}");
 
-            var l = typeof(CurrencyEnum).GetEnumValues();
-            foreach (var obj in l)
-            {
-                Console.WriteLine(obj);
-            }
+            int myInt = 100_000;
+            CashEnum cashEnum = (CashEnum)myInt;
+            Console.WriteLine($"myInt: {myInt} - cashEnum: {cashEnum}");
+            CashEnum foo = (CashEnum)Enum.ToObject(typeof(CashEnum) , myInt);
+            CashEnum foo2 = (CashEnum)Enum.Parse(typeof(CashEnum), 5_000.ToString());
+            Console.WriteLine($"foo: {foo}");
+            Console.WriteLine($"foo2: {(int)foo2:N0}");
+
 
         }
+
+        private static bool CheckEnum(CashEnum cashEnum)
+        {
+            return (int)cashEnum == Convert.ToInt32(CashEnum.HundredThousand);
+        }
+        
+        private enum CashEnum : int
+        {
+            HundredThousand = 100_000,
+            FiftyThousand = 50_000,
+            TwentyThousand = 20_000,
+            TenThousand = 10_000,
+            FiveThousand = 5_000,
+            OneThousand = 1_000
+        }
     }
+    /*
+     C# – How to cast int to enum
+     From an int:
+
+        YourEnum foo = (YourEnum)yourInt;
+
+    From a string:
+
+        YourEnum foo = (YourEnum) Enum.Parse(typeof(YourEnum), yourString);
+
+        // The foo.ToString().Contains(",") check is necessary for enumerations marked with an [Flags] attribute
+        if (!Enum.IsDefined(typeof(YourEnum), foo) && !foo.ToString().Contains(","))
+        {
+            throw new InvalidOperationException($"{yourString} is not an underlying value of the YourEnum enumeration.")
+        }
+
+    Update:
+
+    From number you can also
+
+        YourEnum foo = (YourEnum)Enum.ToObject(typeof(YourEnum) , yourInt);
+     
+     */
 }
